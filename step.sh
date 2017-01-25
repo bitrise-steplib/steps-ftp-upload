c08d30bdf958a14db571d5fc87dabbbd9e248cab
trapacska <trapacska@gmail.com>
2017-01-25 21:08:04 +0100

    code clean-up, close #10

--- step.sh
+++ step.sh
@@ -54,12 +54,21 @@ function validate_required_input {
 	fi
 }
 
+function print_secret_value {
+  if ! [ -z $1 ] ; then 
+	  echo "***" 
+  fi
+}
+
 #=======================================
 # Main
 #=======================================
 
 # Validate parameters
 echo_info "Configs:"
+echo_details "* hostname: $hostname"
+echo_details "* username: $(print_secret_value $username)"
+echo_details "* password: $(print_secret_value $password)"
 echo_details "* upload_source_path: $upload_source_path"
 echo_details "* upload_target_path: $upload_target_path"
 
